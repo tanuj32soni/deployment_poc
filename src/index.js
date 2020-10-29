844f09814366ff4916ba3c00bfb90c365904a065
Tanuj <tanuj32soni@gmail.com>
2020-10-29 19:25:03 +0530

    Added API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var express = require('express');
+var cors = require('cors')
+var app = express()
+const logger = require('morgan')
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+app.use(cors())
+app.use(logger('dev'))
+app.get('/api', function (req, res) {
+  res.send({ message: 'Hello World!'});
+});
+
+app.get('/healthcheck', function (req, res) {
+  res.send("test");
+});
+
+var server = app.listen(4000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
